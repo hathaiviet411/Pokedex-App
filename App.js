7524b0987ccb2bb039c741148d09b1ad45cd29de
hathaiviet411 <hathaiviet411@gmail.com>
2023-04-04 23:09:50 +0700

    [INIT BASE]

--- App.js
+++ App.js
@@ -1,10 +1,57 @@
+import { useState, useEffect, useCallback } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import React from 'react';
+import * as Font from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
+
+const fetchFonts = () =>
+  Font.loadAsync({
+    'Cookie-Regular': require('./assets/fonts/Cookie-Regular.ttf'),
+  });
+
+export default function App(props) {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Pre-load fonts
+        await fetchFonts();
+        // Artificially delay for two seconds to simulate a slow loading
+        await new Promise(resolve => setTimeout(resolve, 5000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Text style={styles.text}>Pokédex Application</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +64,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  text: {
+    fontSize: 40,
+    fontFamily: "Cookie-Regular",
+  }
 });
