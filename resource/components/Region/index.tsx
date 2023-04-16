9e6215af9b00ce0a2054b358a96d1a7f09630c2f
Saabirose <hathaiviet411@gmail.com>
2023-04-16 17:11:59 +0700

    [UPDATE]

--- resource/components/Region/index.tsx
+++ resource/components/Region/index.tsx
@@ -1,11 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { StyleSheet, Text, View } from 'react-native';
+import regions from './regions';
+
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity, Image } from 'react-native';
 
 export default function Region() {
+  const [hasScrollDown, setHasScrollDown] = useState(false);
+
+  const handleScroll = (event) => {
+    const scrollPosition = event.nativeEvent.contentOffset.y;
+
+    console.log(scrollPosition);
+
+    if (scrollPosition > 30) {
+      setHasScrollDown(true);
+    } else {
+      setHasScrollDown(false);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Region</Text>
+    <View style={[styles.container, { paddingTop: hasScrollDown ? 0 : 20 }]}>
+      <ScrollView onScroll={handleScroll} scrollEventThrottle={5}>
+        {regions.map((region, index) => (
+          <TouchableOpacity key={index} style={{ gap: 20 }}>
+            <Image
+              source={{ uri: region.region_image }}
+              style={{
+                width: '100%',
+                height: 150,
+                borderRadius: 15,
+                resizeMode: 'stretch',
+              }}
+            />
+
+            <View style={styles.transparentBackground}>
+
+            </View>
+
+            <View style={styles.regionItem}>
+              <View style={styles.regionItemLeft}>
+                <Text style={styles.regionItemName}>{region.name}</Text>
+                <Text style={styles.regionItemGeneration}>{region.generation}</Text>
+              </View>
+
+              <View style={styles.regionItemRight}>
+                {
+                  region.pokemons.map((pokemon, pokemonIndex) => (
+                    <Image
+                      key={pokemonIndex}
+                      source={{ uri: pokemon.avatar }}
+                      style={{
+                        width: 80,
+                        height: 80,
+                        resizeMode: 'cover',
+                      }}
+                    />
+                  ))
+                }
+              </View>
+            </View>
+          </TouchableOpacity>
+        ))
+        }
+      </ScrollView>
     </View>
   )
 }
@@ -13,6 +71,48 @@ export default function Region() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingBottom: 40,
+    paddingHorizontal: 10,
     backgroundColor: '#fff',
-  }
-})
+  },
+
+  transparentBackground: {
+    top: 0,
+    height: 150,
+    opacity: 0.5,
+    width: '100%',
+    borderRadius: 15,
+    position: 'absolute',
+    backgroundColor: '#000',
+  },
+
+  regionItem: {
+    top: 0,
+    flexDirection: 'row',
+    position: 'absolute',
+  },
+
+  regionItemLeft: {
+    gap: 5,
+    paddingTop: 60,
+    paddingLeft: 30,
+  },
+
+  regionItemName: {
+    fontSize: 24,
+    color: '#FFF',
+    fontWeight: '900',
+  },
+
+  regionItemGeneration: {
+    fontSize: 16,
+    color: '#F5F5F7',
+    fontWeight: '700',
+  },
+
+  regionItemRight: {
+    paddingTop: 40,
+    paddingLeft: 30,
+    flexDirection: 'row',
+  },
+});
--- resource/components/Region/regions.ts
+++ resource/components/Region/regions.ts
@@ -2,160 +2,160 @@ export default [
   {
     id: 1,
     name: 'Kanto',
-    region_image: '',
+    region_image: 'https://github.com/nonamelittlefox/Pokedex-App/blob/develop/public/images/regions/kanto.png?raw=true',
     generation: '1º Generation',
     pokemons: [
       {
         id: 1,
-        avatar: '',
+        avatar: 'https://github.com/nonamelittlefox/Pokedex-App/blob/develop/public/images/regions/kanto_1.png?raw=true',
       },
       {
         id: 2,
-        avatar: '',
+        avatar: 'https://github.com/nonamelittlefox/Pokedex-App/blob/develop/public/images/regions/kanto_2.png?raw=true',
       },
       {
         id: 3,
-        avatar: '',
+        avatar: 'https://github.com/nonamelittlefox/Pokedex-App/blob/develop/public/images/regions/kanto_3.png?raw=true',
       },
     ]
   },
   {
     id: 2,
     name: 'Johto',
-    region_image: '',
+    region_image: 'https://github.com/nonamelittlefox/Pokedex-App/blob/develop/public/images/regions/johto.png?raw=true',
     generation: '2º Generation',
     pokemons: [
       {
         id: 1,
-        avatar: '',
+        avatar: 'https://github.com/nonamelittlefox/Pokedex-App/blob/develop/public/images/regions/johto_1.png?raw=true',
       },
       {
         id: 2,
-        avatar: '',
+        avatar: 'https://github.com/nonamelittlefox/Pokedex-App/blob/develop/public/images/regions/johto_2.png?raw=true',
       },
       {
         id: 3,
-        avatar: '',
+        avatar: 'https://github.com/nonamelittlefox/Pokedex-App/blob/develop/public/images/regions/johto_3.png?raw=true',
       },
     ]
   },
   {
     id: 3,
     name: 'Hoenn',
-    region_image: '',
+    region_image: 'https://github.com/nonamelittlefox/Pokedex-App/blob/develop/public/images/regions/hoenn.png?raw=true',
     generation: '3º Generation',
     pokemons: [
       {
         id: 1,
-        avatar: '',
+        avatar: 'https://github.com/nonamelittlefox/Pokedex-App/blob/develop/public/images/regions/hoenn_1.png?raw=true',
       },
       {
         id: 2,
-        avatar: '',
+        avatar: 'https://github.com/nonamelittlefox/Pokedex-App/blob/develop/public/images/regions/hoenn_2.png?raw=true',
       },
       {
         id: 3,
-        avatar: '',
+        avatar: 'https://github.com/nonamelittlefox/Pokedex-App/blob/develop/public/images/regions/hoenn_3.png?raw=true',
       },
     ]
   },
   {
     id: 4,
     name: 'Sinnoh',
-    region_image: '',
+    region_image: 'https://github.com/nonamelittlefox/Pokedex-App/blob/develop/public/images/regions/sinnoh.png?raw=true',
     generation: '4º Generation',
     pokemons: [
       {
         id: 1,
-        avatar: '',
+        avatar: 'https://github.com/nonamelittlefox/Pokedex-App/blob/develop/public/images/regions/sinnoh_1.png?raw=true',
       },
       {
         id: 2,
-        avatar: '',
+        avatar: 'https://github.com/nonamelittlefox/Pokedex-App/blob/develop/public/images/regions/sinnoh_2.png?raw=true',
       },
       {
         id: 3,
-        avatar: '',
+        avatar: 'https://github.com/nonamelittlefox/Pokedex-App/blob/develop/public/images/regions/sinnoh_3.png?raw=true',
       },
     ]
   },
   {
     id: 5,
     name: 'Unova',
-    region_image: '',
+    region_image: 'https://github.com/nonamelittlefox/Pokedex-App/blob/develop/public/images/regions/unova.png?raw=true',
     generation: '5º Generation',
     pokemons: [
       {
         id: 1,
-        avatar: '',
+        avatar: 'https://github.com/nonamelittlefox/Pokedex-App/blob/develop/public/images/regions/unova_1.png?raw=true',
       },
       {
         id: 2,
-        avatar: '',
+        avatar: 'https://github.com/nonamelittlefox/Pokedex-App/blob/develop/public/images/regions/unova_2.png?raw=true',
       },
       {
         id: 3,
-        avatar: '',
+        avatar: 'https://github.com/nonamelittlefox/Pokedex-App/blob/develop/public/images/regions/unova_3.png?raw=true',
       },
     ]
   },
   {
     id: 6,
     name: 'Kalos',
-    region_image: '',
+    region_image: 'https://github.com/nonamelittlefox/Pokedex-App/blob/develop/public/images/regions/kalos.png?raw=true',
     generation: '6º Generation',
     pokemons: [
       {
         id: 1,
-        avatar: '',
+        avatar: 'https://github.com/nonamelittlefox/Pokedex-App/blob/develop/public/images/regions/kalos_1.png?raw=true',
       },
       {
         id: 2,
-        avatar: '',
+        avatar: 'https://github.com/nonamelittlefox/Pokedex-App/blob/develop/public/images/regions/kalos_2.png?raw=true',
       },
       {
         id: 3,
-        avatar: '',
+        avatar: 'https://github.com/nonamelittlefox/Pokedex-App/blob/develop/public/images/regions/kalos_3.png?raw=true',
       },
     ]
   },
   {
     id: 7,
     name: 'Alola',
-    region_image: '',
+    region_image: 'https://github.com/nonamelittlefox/Pokedex-App/blob/develop/public/images/regions/aloha.png?raw=true',
     generation: '7º Generation',
     pokemons: [
       {
         id: 1,
-        avatar: '',
+        avatar: 'https://github.com/nonamelittlefox/Pokedex-App/blob/develop/public/images/regions/aloha_1.png?raw=true',
       },
       {
         id: 2,
-        avatar: '',
+        avatar: 'https://github.com/nonamelittlefox/Pokedex-App/blob/develop/public/images/regions/aloha_2.png?raw=true',
       },
       {
         id: 3,
-        avatar: '',
+        avatar: 'https://github.com/nonamelittlefox/Pokedex-App/blob/develop/public/images/regions/aloha_3.png?raw=true',
       },
     ]
   },
   {
     id: 8,
     name: 'Galar',
-    region_image: '',
+    region_image: 'https://github.com/nonamelittlefox/Pokedex-App/blob/develop/public/images/regions/galar.png?raw=true',
     generation: '8º Generation',
     pokemons: [
       {
         id: 1,
-        avatar: '',
+        avatar: 'https://github.com/nonamelittlefox/Pokedex-App/blob/develop/public/images/regions/galar_1.png?raw=true',
       },
       {
         id: 2,
-        avatar: '',
+        avatar: 'https://github.com/nonamelittlefox/Pokedex-App/blob/develop/public/images/regions/galar_2.png?raw=true',
       },
       {
         id: 3,
-        avatar: '',
+        avatar: 'https://github.com/nonamelittlefox/Pokedex-App/blob/develop/public/images/regions/galar_3.png?raw=true',
       },
     ]
   },
